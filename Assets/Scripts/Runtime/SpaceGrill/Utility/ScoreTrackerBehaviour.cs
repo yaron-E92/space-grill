0e5e9e17cec6fbe221f74e645f3cbe00f0d407a7
Yaron Efrat <yaronefrat92@gmail.com>
2024-06-08 22:49:37 +0200

    [6] refactor: Put magic numbers in consts and correct game settings

--- Assets/Scripts/Runtime/SpaceGrill/Utility/ScoreTrackerBehaviour.cs
+++ Assets/Scripts/Runtime/SpaceGrill/Utility/ScoreTrackerBehaviour.cs
@@ -7,18 +7,22 @@ namespace SpaceGrill.Utility
     {
         public int Score {  get; private set; } = 0;
 
+        [SerializeField]
+        private TMPro.TextMeshPro _textField;
+
         private string ScoreText => $"Score: {Score}";
 
-        public TMPro.TextMeshPro _textField;
+        private const int TopRightCornerXTranslate = 53;
+        private const int FontSize = 2000;
 
         [Inject]
         public void Construct()
         {
             _textField.fontStyle = TMPro.FontStyles.Bold;
-            _textField.fontSize = 2000;
+            _textField.fontSize = FontSize;
             _textField.text = ScoreText;
             _textField.sortingOrder = 1;
-            gameObject.transform.Translate(new Vector3(53, 0, 0));
+            gameObject.transform.Translate(new Vector3(TopRightCornerXTranslate, 0, 0));
         }
 
         public void IncrementScore()
