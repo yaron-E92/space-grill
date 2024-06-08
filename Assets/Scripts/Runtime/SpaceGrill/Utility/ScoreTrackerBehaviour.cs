21f5d0b511ab03a3b202d93f79b50da360685b39
Yaron Efrat <yaronefrat92@gmail.com>
2024-06-08 23:10:20 +0200

    [6] fix: Add a conditional compilation alternative to TMPRO
    In this commit, we provide a conditional compilation branching in
    `ScoreTrackerBehaviour`, an approach we wanted to abandon, however the
    Github agent wouldn't have access to TextMeshPro, and therefore we must
    provide the alternative compilation using Unity UI's `Text`.
    
    We add a Define constant to the csproj if it can find the TextMeshPro dll,
    and that is what we use in the conditional compilation.

--- Assets/Scripts/Runtime/SpaceGrill/Utility/ScoreTrackerBehaviour.cs
+++ Assets/Scripts/Runtime/SpaceGrill/Utility/ScoreTrackerBehaviour.cs
@@ -8,20 +8,32 @@ namespace SpaceGrill.Utility
         public int Score {  get; private set; } = 0;
 
         [SerializeField]
+#if TMPROEXISTS
         private TMPro.TextMeshPro _textField;
+        private const int FontSize = 2000;
+        private const int TopRightCornerXTranslate = 53;
+#else
+        private UnityEngine.UI.Text _textField;
+        private const int FontSize = 257;
+        private const int TopRightCornerXTranslate = 52;
+#endif
 
         private string ScoreText => $"Score: {Score}";
-
-        private const int TopRightCornerXTranslate = 53;
-        private const int FontSize = 2000;
 
         [Inject]
         public void Construct()
         {
+#if TMPROEXISTS
+            _textField = gameObject.AddComponent<TMPro.TextMeshPro>();
             _textField.fontStyle = TMPro.FontStyles.Bold;
+            _textField.sortingOrder = 1;
+#else
+            _textField = gameObject.AddComponent<UnityEngine.UI.Text>();
+            _textField.font = Font.CreateDynamicFontFromOSFont("LiberationSans", FontSize);
+            _textField.fontStyle = UnityEngine.FontStyle.Bold;
+#endif
             _textField.fontSize = FontSize;
             _textField.text = ScoreText;
-            _textField.sortingOrder = 1;
             gameObject.transform.Translate(new Vector3(TopRightCornerXTranslate, 0, 0));
         }
 
